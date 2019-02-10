3ce458a89ef617ea4f69956f552fc7820ffebd40
Kyle C <kyclutter@gmail.com>
2019-02-10 17:34:29 -0600

    main page

---
+++ src/Components/MainPage/MainPage.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+
+
+
+class MainPage extends Component {
+
+
+    render(){
+
+
+
+        return(
+            <div className="header">
+                <div className = 'header__text-box'>
+                    <h1 className='heading-primary'>
+                    <span className='heading-primary--main first'> 
+                        Kyle
+                    </span>
+                    <span className='middle-int'>W.</span>
+                    <span className='heading-primary--main2'>
+                         Clutter 
+                        </span> 
+                        <span className='heading-primary--sub'>Front-end Web Developer</span>
+
+                    
+                    
+                    </h1>
+
+
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+export default MainPage;
