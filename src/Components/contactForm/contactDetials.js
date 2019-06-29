32e1e45ed83fb0e2500a09f877631db02b03bb0f
Kyle C <kyclutter@gmail.com>
2019-06-29 13:20:13 -0500

    updates

--- src/Components/contactForm/contactDetials.js
+++ src/Components/contactForm/contactDetials.js
@@ -1,73 +1,56 @@
-import React from 'react';
+import React from "react";
 
+const contactDetials = props => {
+  return (
+    <form className="form">
+      <div className="u-margin-bottom-medium">
+        <h3 className="heading-secondary">Let's Work Together</h3>
+      </div>
+      <div className="form__group">
+        <input
+          className="form__input"
+          onChange={props.nameChange}
+          type="text"
+          value={props.name}
+          placeholder="Name"
+          name="name"
+        />
+        <label className="form__label">Name</label>
+      </div>
+      <div className="form__group">
+        <input
+          type="email"
+          className="form__input"
+          placeholder="Email Address"
+          value={props.email}
+          onChange={props.emailChange}
+          name="email"
+        />
+        <label className="form__label">Email Address</label>
+      </div>
+      <div className="form__group-text">
+        <textarea
+          className="form__input"
+          value={props.message}
+          placeholder="Message"
+          onChange={props.messageChange}
+          name="message"
+        />
+        <label className="form__label">Message</label>
+      </div>
+      <div className="btn__container-form">
+        <button
+          className="btn-form"
+          type="submit"
+          value="Send"
+          disabled={!props.valid}
+          onClick={props.submit}
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
 
-
-
-
-
-const contactDetials = (props) => {
-
-
-    return (
-        <form className="form">
-            <div className="u-margin-bottom-medium">
-                <h3 className="heading-secondary">
-                    Let's Work Together
-                </h3>
-            </div>
-            <div className="form__group">
-                <input 
-                className="form__input"
-                onChange = {props.nameChange}
-                type ="text"
-                value = {props.name}
-                placeholder="Name" 
-                name="name"
-                />
-                <label
-                className = "form__label">Name</label>
-                
-            </div>
-            <div className="form__group">
-                <input 
-                type="email" 
-                className="form__input" 
-                placeholder="Email Address"
-                value = {props.email}
-                onChange = {props.emailChange}
-                name ="email" />
-                <label
-                className = "form__label">Email Address</label>
-                
-                
-            </div>
-            <div className="form__group-text">
-        
-                <textarea 
-                className="form__input" 
-                value = {props.message} 
-                placeholder="Message"
-                onChange={props.messageChange} 
-                name="message"/>
-                <label
-                className = "form__label">Message</label>
-                
-                
-            </div>
-            <div className = "btn__container-form">
-                <button 
-                className = "btn-form"
-                type = "submit"
-                value="Send"
-                disabled ={!props.valid}
-                onClick = {props.submit}
-                >Submit</button>
-            </div>
-           
-        </form>
-
-    )
-}
-
-
-export default contactDetials; 
+export default contactDetials;
